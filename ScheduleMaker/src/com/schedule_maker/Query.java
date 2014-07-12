09bc94eea08ca5ec8719b5e70f7b9e6c47333b58
Isaac <irbowen@umich.edu>
2014-07-11 20:00:05 -0400

    Fixed things

--- ScheduleMaker/src/com/schedule_maker/Query.java
+++ ScheduleMaker/src/com/schedule_maker/Query.java
@@ -1,5 +1,6 @@
 package com.schedule_maker;
 
+
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -22,16 +23,62 @@ public class Query extends DatabaseConnection {
 	 */
 	public ArrayList<String> getDepartments() {
 		String query = "SELECT DISTINCT Subject_Code FROM CLASSES ORDER BY Subject_Code;";
+		System.out.println(query);
 		return getQueryResults(query, "Subject_Code");
 	}
 
 	/*
 	 * Returns all courses within a department, given its cdoe, as an arraylist of strings
-	 * On failure, returns an empty list
 	 */
-	public ArrayList<String> getCourses(String code) {
-		String query = "SELECT DISTINCT Catalog_Num FROM CLASSES WHERE Subject_Code = '" + code + "' ORDER BY Catalog_Num;";
+	public ArrayList<String> getCourses(String dept_code) {
+		String query = "SELECT DISTINCT Catalog_Num FROM CLASSES WHERE Subject_Code = '" 
+				+ dept_code + "' ORDER BY Catalog_Num;";
 		return getQueryResults(query, "Catalog_Num");
+	}
+	
+	/*
+	 * Returns all courses within a department, given its cdoe, as an arraylist of strings
+	 */
+	public ArrayList<String> getAllComponents(String dept_code, String catalog_num) {
+		String query = "SELECT Class_Number FROM CLASSES WHERE Subject_Code = '" + dept_code + 
+						"' AND Catalog_Num = '" + catalog_num + "'";
+		return getQueryResults(query, "Class_Number");
+	}
+	
+	/*
+	 * Returns the course numbers for each lecture section of the given class
+	 */
+	public ArrayList<String> getLectures(String dept_code, String catalog_num) {
+		String query = "SELECT Class_Number FROM CLASSES WHERE Subject_Code = '" + dept_code + 
+						"' AND Catalog_Num = '" + catalog_num + "' AND Component = 'LEC'";
+		return getQueryResults(query, "Class_Number");
+	}
+	
+	/*
+	 * Returns the course numbers for each lab section of the given class
+	 */
+	public ArrayList<String> getLabratories(String dept_code, String catalog_num) {
+		String query = "SELECT Class_Number FROM CLASSES WHERE Subject_Code = '" + dept_code + 
+						"' AND Catalog_Num = '" + catalog_num + "' AND Component = 'LAB'";
+		return getQueryResults(query, "Class_Number");
+	}
+	
+	/*
+	 * Returns the course numbers for each discussion section of the given class
+	 */
+	public ArrayList<String> getDiscussions(String dept_code, String catalog_num) {
+		String query = "SELECT Class_Number FROM CLASSES WHERE Subject_Code = '" + dept_code + 
+						"' AND Catalog_Num = '" + catalog_num + "' AND Component = 'DIS'";
+		return getQueryResults(query, "Class_Number");
+	}
+	
+	/*
+	 * Returns the course numbers for each recitation section of the given class
+	 */
+	public ArrayList<String> getRecitations(String dept_code, String catalog_num) {
+		String query = "SELECT Class_Number FROM CLASSES WHERE Subject_Code = '" + dept_code + 
+						"' AND Catalog_Num = '" + catalog_num + "' AND Component = 'REC'";
+		return getQueryResults(query, "Class_Number");
 	}
 	
 	/*
--- ScheduleMaker/src/com/schedule_maker/ScheduleMaker.java
+++ ScheduleMaker/src/com/schedule_maker/ScheduleMaker.java
@@ -29,10 +29,10 @@ public class ScheduleMaker {
 		return json;
 	}
 	
-	@GET
 	/*
 	 * Just for initializing db
 	 */
+	@GET
 	public Response getMsg() {
 	//	DatabaseConnection conn = new DatabaseConnection();conn.createTables();Parse myParse = new Parse();myParse.parseFile();
 		return Response.status(200).entity("Doing things").build(); 
@@ -55,12 +55,78 @@ public class ScheduleMaker {
 	 * Returns the list of all departments in JSOM form
 	 * TODO: Change @Produces to application/json after testing
 	 */
-	@Path("/departments/{code}/classes")
+	@Path("/departments/{dept_code}/classes")
 	@GET
 	@Produces("text/html")
-	public Response getCoursesByDeparment(@PathParam("code") String code) {
+	public Response getCoursesByDeparment(@PathParam("dept_code") String dept_code) {
 		Query q = new Query();
-		JSONObject json = toJSON(q.getCourses(code));
+		JSONObject json = toJSON(q.getCourses(dept_code));
+		
+		return Response.status(200).entity(json.toString()).build(); 
+	}
+	
+	/*
+	 * Returns the list of all lecture course numbers
+	 * TODO: Change @Produces to application/json after testing
+	 */
+	@Path("/departments/{dept_code}/classes/{catalog_num}")
+	@GET
+	@Produces("text/html")
+	public Response getAllComponents(@PathParam("dept_code") String dept_code, @PathParam("catalog_num") String catalog_num) {
+		Query q = new Query();
+		JSONObject json = toJSON(q.getAllComponents(dept_code, catalog_num));
+		return Response.status(200).entity(json.toString()).build(); 
+	}
+	
+	/*
+	 * Returns the list of all lecture course numbers
+	 * TODO: Change @Produces to application/json after testing
+	 */
+	@Path("/departments/{dept_code}/classes/{catalog_num}/lec")
+	@GET
+	@Produces("text/html")
+	public Response getLectures(@PathParam("dept_code") String dept_code, @PathParam("catalog_num") String catalog_num) {
+		Query q = new Query();
+		JSONObject json = toJSON(q.getLectures(dept_code, catalog_num));
+		return Response.status(200).entity(json.toString()).build(); 
+	}
+	
+	/*
+	 * Returns the list of all departments in JSOM form
+	 * TODO: Change @Produces to application/json after testing
+	 */
+	@Path("/departments/{dept_code}/classes/{catalog_num}/lab")
+	@GET
+	@Produces("text/html")
+	public Response getLabratories(@PathParam("dept_code") String dept_code, @PathParam("catalog_num") String catalog_num) {
+		Query q = new Query();
+		JSONObject json = toJSON(q.getLabratories(dept_code, catalog_num));
+		return Response.status(200).entity(json.toString()).build(); 
+	}
+	
+	/*
+	 * Returns the list of all departments in JSOM form
+	 * TODO: Change @Produces to application/json after testing
+	 */
+	@Path("/departments/{dept_code}/classes/{catalog_num}/dis")
+	@GET
+	@Produces("text/html")
+	public Response getDiscussions(@PathParam("dept_code") String dept_code, @PathParam("catalog_num") String catalog_num) {
+		Query q = new Query();
+		JSONObject json = toJSON(q.getDiscussions(dept_code, catalog_num));
+		return Response.status(200).entity(json.toString()).build(); 
+	}
+	
+	/*
+	 * Returns the list of all departments in JSOM form
+	 * TODO: Change @Produces to application/json after testing
+	 */
+	@Path("/departments/{dept_code}/classes/{catalog_num}/rec")
+	@GET
+	@Produces("text/html")
+	public Response getRecitations(@PathParam("dept_code") String dept_code, @PathParam("catalog_num") String catalog_num) {
+		Query q = new Query();
+		JSONObject json = toJSON(q.getRecitations(dept_code, catalog_num));
 		return Response.status(200).entity(json.toString()).build(); 
 	}
 }
